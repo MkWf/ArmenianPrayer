9168ff65c0f05c6e503e197479e402fc529f66b3
MkWf <markdevw@gmail.com>
2021-11-23 12:52:37 -0500

    Change text functionality
    Added functionality to dropdown menu for changing the languages

--- App.js
+++ App.js
@@ -2,41 +2,122 @@ import React, { useState } from 'react';
 import {StyleSheet, Text, View, SafeAreaView, Platform, StatusBar, Picker} from 'react-native';
 
 
-const App = () => {
-  const [selectedValue, setSelectedValue] = useState("java");
+export default function App () {
 
-  return (
-    <SafeAreaView style={styles.screen}>
-      <Picker selectedValue={selectedValue}
-        onValueChange={(itemValue) => setSelectedValue(itemValue)}>
+  const [selectedValue, setSelectedValue] = useState("arm_tra_eng");
+
+  if(selectedValue == "arm_tra_eng"){
+    return (
+      <SafeAreaView style={styles.screen}>
+        <Picker 
+          selectedValue={selectedValue}
+          onValueChange={(itemValue) => setSelectedValue(itemValue)}>
         
-        <Picker.Item label="Armenian | Transliteration | English" value="arm_tra_eng" />
-        <Picker.Item label="Armenian | Transliteration" value="arm_tra" />
-        <Picker.Item label="Armenian" value="arm" />
-        <Picker.Item label="English" value="eng" />
-      </Picker>
-    
-      <View style={styles.screenTextContainer}>
-        <View style={styles.languageContainer}>
-          <Text style={styles.armenianText}>
-            My armenian text. My armenian text. My armenian text
-          </Text>
-        </View>
+          <Picker.Item label="Armenian | Transliteration | English" value="arm_tra_eng" />
+          <Picker.Item label="Armenian | Transliteration" value="arm_tra" />
+          <Picker.Item label="Armenian" value="arm" />
+        </Picker>
         
-        <View style={styles.languageContainer}>
-          <Text style={styles.armenianText}>
-            My translit text. My translit text. My translit text
-          </Text>
+        <View style={styles.screenTextContainer}>
+          <View style={styles.languageContainer}>
+            <Text style={styles.text}>
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+            </Text>
+          </View>
+
+          <View style={styles.languageContainer}>
+            <Text style={styles.text}>
+              My translit text. My translit text. My translit text
+              My translit text. My translit text. My translit text
+              My translit text. My translit text. My translit text
+              My translit text. My translit text. My translit text
+              My translit text. My translit text. My translit text
+              My translit text. My translit text. My translit text
+            </Text>
+          </View>
+
+          <View style={styles.languageContainer}>
+            <Text style={styles.text}>
+              My english text. My english text. My english text
+              My english text. My english text. My english text
+              My english text. My english text. My english text
+              My english text. My english text. My english text
+              My english text. My english text. My english text
+              My english text. My english text. My english text
+            </Text>
+          </View>
         </View>
+      </SafeAreaView>
+    )
+  }else if(selectedValue == "arm_tra"){
+    return (
+      <SafeAreaView style={styles.screen}>
+        <Picker 
+          selectedValue={selectedValue}
+          onValueChange={(itemValue) => setSelectedValue(itemValue)}>
         
-        <View style={styles.languageContainer}>
-          <Text style={styles.armenianText}>
-            My english text. My english text. My english text
-          </Text>
+          <Picker.Item label="Armenian | Transliteration | English" value="arm_tra_eng" />
+          <Picker.Item label="Armenian | Transliteration" value="arm_tra" />
+          <Picker.Item label="Armenian" value="arm" />
+        </Picker>
+        
+        <View style={styles.screenTextContainer}>
+          <View style={styles.languageContainer}>
+            <Text style={styles.text}>
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+            </Text>
+          </View>
+
+          <View style={styles.languageContainer}>
+            <Text style={styles.text}>
+              My translit text. My translit text. My translit text
+              My translit text. My translit text. My translit text
+              My translit text. My translit text. My translit text
+              My translit text. My translit text. My translit text
+              My translit text. My translit text. My translit text
+              My translit text. My translit text. My translit text
+            </Text>
+          </View>
         </View>
-      </View>
-    </SafeAreaView>
-  );
+      </SafeAreaView>
+    )
+  }else if(selectedValue == "arm"){
+    return (
+      <SafeAreaView style={styles.screen}>
+        <Picker 
+          selectedValue={selectedValue}
+          onValueChange={(itemValue) => setSelectedValue(itemValue)}>
+        
+          <Picker.Item label="Armenian | Transliteration | English" value="arm_tra_eng" />
+          <Picker.Item label="Armenian | Transliteration" value="arm_tra" />
+          <Picker.Item label="Armenian" value="arm" />
+        </Picker>
+        
+        <View style={styles.screenTextContainer}>
+          <View style={styles.languageContainer}>
+            <Text style={styles.text}>
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+              My armenian text. My armenian text. My armenian text
+            </Text>
+          </View>
+        </View>
+      </SafeAreaView>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -54,15 +135,7 @@ const styles = StyleSheet.create({
     padding: 4,
     flexGrow: 1
   },
-  armenianText: {
-    fontSize: 20,
-  },
-  translitText: {
-    fontSize: 20,
-  },
-  englishText: {
+  text: {
     fontSize: 20,
   }
 });
-
-export default App;
