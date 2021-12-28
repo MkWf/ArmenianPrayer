7a2c5beb58870f121cc1fbe8241df81c17dea578
MkWf <markdevw@gmail.com>
2021-12-28 14:55:54 -0500

    sql testing

--- App.js
+++ App.js
@@ -7,112 +7,13 @@ import { Asset } from 'expo-asset';
 import HTML from 'react-native-render-html';
 
 
-
 export default function App() {
     const [items, setItems] = React.useState(null);
+    const [textHtml, setHTML] = React.useState('');
     const { width } = useWindowDimensions();
-    const FOO = 'table.db'
+    const FOO = 'table.db';
 
-    /*<tr>
-        <td align="left">
-            Պատարագ Հայաստանեայց Եկեղեցւոյ
-        </td>
-        <td align="left">
-            Badarak Hayasdanyayts Yegeghetsvo
-        </td>
-        <td align="left">
-            The Divine Liturgy of the Armenian Church
-        </td>
-    </tr>*/
-    const loadDB = async () => {
-      try {
-        if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
-          await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
-        };
-
-        await FileSystem.downloadAsync(Asset.fromModule(require('./asset/table.db')).uri, FileSystem.documentDirectory + `SQLite/${FOO}`).
-          then(() => {
-            const db = SQLite.openDatabase(FOO);
-            db.transaction((tx) => {
-              tx.executeSql(
-                `select * from text`,
-                [],
-                (_, result) => {
-                  //console.log(result); //whole thing
-                 // console.log(result.rows._array); //objects only
-                 const resultRows = result.rows._array;
-                 let i = 0;
-                 console.log(resultRows[i]);
-                 console.log(html);
-                 for(;resultRows[i];){
-                   html += resultRows[i];
-                   i++;
-                   console.log(html);
-                 }
-                },
-                (_, err) => {
-                    console.log("error");
-                });
-              });
-            });
-        } catch (error) {
-          alert("FAIL");
-          console.log("fail");
-          throw new Error(error);
-        }
-    }
-
-    loadDB();
-
-    return (
-      <ScrollView style={styles.container}>
-        <Text>123</Text>
-        <HTML source={{html}} contentWidth={width} />
-      </ScrollView>
-    );
-
-    
-}
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#fff",
-    flex: 1,
-    paddingTop: Constants.statusBarHeight,
-  },
-  heading: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  flexRow: {
-    flexDirection: "row",
-  },
-  input: {
-    borderColor: "#4630eb",
-    borderRadius: 4,
-    borderWidth: 1,
-    flex: 1,
-    height: 48,
-    margin: 16,
-    padding: 8,
-  },
-  listArea: {
-    backgroundColor: "#f0f0f0",
-    flex: 1,
-    paddingTop: 16,
-  },
-  sectionContainer: {
-    marginBottom: 16,
-    marginHorizontal: 16,
-  },
-  sectionHeading: {
-    fontSize: 18,
-    marginBottom: 8,
-  },
-});
-
-let html = `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/TR/REC-html40/strict.dtd">
+    let html = `
 <html>
     <style type="text/css">
         table
@@ -181,4 +82,113 @@ let html = `<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0//EN" "http://www.w3.org/
                 <td colspan="3" align="center"></td>
             </tr>
 `;
+
+    /*<tr>
+        <td align="left">
+            Պատարագ Հայաստանեայց Եկեղեցւոյ
+        </td>
+        <td align="left">
+            Badarak Hayasdanyayts Yegeghetsvo
+        </td>
+        <td align="left">
+            The Divine Liturgy of the Armenian Church
+        </td>
+    </tr>*/
+
+    const loadDB = async () => {
+      try {
+        if (!(await FileSystem.getInfoAsync(FileSystem.documentDirectory + 'SQLite')).exists) {
+          await FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'SQLite');
+        };
+
+        await FileSystem.downloadAsync(Asset.fromModule(require('./asset/table.db')).uri, FileSystem.documentDirectory + `SQLite/${FOO}`).
+          then(() => {
+            const db = SQLite.openDatabase(FOO);
+            db.transaction((tx) => {
+              tx.executeSql(
+                `select * from text`,
+                [], 
+                (_, result) => {
+                  let i;
+                  let rowString;
+                  let rowObject;
+                  console.log("enter");
+                  for(i=0; result.rows._array.length; i++){
+                    console.log(i);
+                    rowString = '<tr><td align="left">';
+                    rowObject = result.rows._array[i];
+                    rowString += rowObject['Ar'];
+                    rowString += '</td><td align="left"';
+                    rowString += rowObject['Tr'];
+                    rowString += '</td><td align="left"';
+                    rowString += rowObject['En'];
+                    rowString += '</td></tr>';
+                    console.log(rowString);
+                  }
+                }, 
+                (_, err) => {
+                    console.log("error");
+                });
+              });
+            }); 
+        } catch (error) {
+          alert("FAIL");
+          console.log("fail");
+          throw new Error(error);
+        }
+    }
+
+    loadDB();
+
+
+
+    return (
+      <ScrollView style={styles.container}>
+        <Text>123</Text>
+        <HTML source={{html}} contentWidth={width} />
+      </ScrollView>
+    );
+
+    
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#fff",
+    flex: 1,
+    paddingTop: Constants.statusBarHeight,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  flexRow: {
+    flexDirection: "row",
+  },
+  input: {
+    borderColor: "#4630eb",
+    borderRadius: 4,
+    borderWidth: 1,
+    flex: 1,
+    height: 48,
+    margin: 16,
+    padding: 8,
+  },
+  listArea: {
+    backgroundColor: "#f0f0f0",
+    flex: 1,
+    paddingTop: 16,
+  },
+  sectionContainer: {
+    marginBottom: 16,
+    marginHorizontal: 16,
+  },
+  sectionHeading: {
+    fontSize: 18,
+    marginBottom: 8,
+  },
+});
+
+
 
