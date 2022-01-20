fc915fd5006cc3caf6c779a25a55d5608c40c17f
MkWf <markdevw@gmail.com>
2022-01-20 16:18:00 -0500

    WIP

--- MainScreen.js
+++ MainScreen.js
@@ -1,28 +1,43 @@
 import React from "react";
-import {StyleSheet, StatusBar, View, Text} from "react-native";
+import {StyleSheet, StatusBar, View, Text, SafeAreaView, Image} from "react-native";
+import { Card } from 'react-native-elements';
 
 export function MainScreen({navigation}) {
     return (
-        <View style={styles.screen}>
-        
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Card style={styles.card} title="Local Modules">
+                <Text style={styles.paragraph}>
+                    Welcome to the Armenian Prayer App 
+                    There's just liturgy for now but more will be coming soon!
+                    Read about upcoming features >
+                </Text>
+            </Card>
+
+            <Card style={styles.card} title="Local Modules">
+                <Image source={require('./assets/Gomidas.png')}></Image>
+                <Text style={styles.paragraph}>
+                    Time to Pray! 
+                    The Divine Liturgy
+                    We pray the liturgy every Sunday to celebrate new life in Jesus 
+                </Text>
+            </Card>
+        </SafeAreaView>
     );
 }
 
-export const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-        backgroundColor: '#ecf0f1',
-        alignItems: "center"
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      backgroundColor: '#ecf0f1',
     },
-    mainHeading: {
-        fontSize: 30,
+    card: {
+        width: '50%'
     },
-    baseText: {
-        fontSize: 20
-    }, 
-    italic: {
-        fontStyle: "italic"
-    }
+    paragraph: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      color: '#34495e',
+    },
 });
