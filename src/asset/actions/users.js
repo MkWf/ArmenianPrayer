167274e130dc35f7715dc496ea79774f7e72736a
MkWf <markdevw@gmail.com>
2022-02-01 12:44:21 -0500

    Updated actions/users
    Added languages

--- src/asset/actions/users.js
+++ src/asset/actions/users.js
@@ -6,3 +6,13 @@ export const UPDATE_USERNAME = 'UPDATE_USERNAME';
 //And we create the corresponding action factory. An action factory is just a function that outputs an object with the type field 
 //and any other data required to update the state.
 export const updateUsername = (username) => ({ type: UPDATE_USERNAME, username });
+
+
+
+export const UPDATE_ARMENIAN = 'UPDATE_ARMENIAN';
+export const UPDATE_TRANSLIT = 'UPDATE_TRANSLIT';
+export const UPDATE_ENGLISH = 'UPDATE_ENGLISH';
+
+export const updateArmenian = (isArmenian) => ({ type: UPDATE_ARMENIAN, isArmenian });
+export const updateTranslit = (isTranslit) => ({ type: UPDATE_TRANSLIT, isTranslit });
+export const updateEnglish = (isEnglish) => ({ type: UPDATE_ENGLISH, isEnglish });
